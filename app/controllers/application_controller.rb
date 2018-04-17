35d4d2134af606b2d3524471fb2b5fe539f005e5
Shistik <38461186+Shistik@users.noreply.github.com>
2018-04-18 01:31:12 +0300

    mm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_locale
+  before_action :set_locale,:default_url_options
   
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.default_locale = params[:locale]|| I18n.default_locale
+    if (params[:new_locale]!= nil)
+      I18n.default_locale = params[:new_locale]
+      I18n.locale = params[:new_locale]
+    end
   end
-  def default_url_options(options = {})
-    { locale: I18n.locale }.merge options
+  def default_url_options
+    { locale: I18n.locale }
   end
-
+  
   before_action :require_login
 
   include SessionsHelper
